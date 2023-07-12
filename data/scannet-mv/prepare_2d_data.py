9212c726c900f19171ac64e98ea71d70b38348a9
MYSKY-github <79625673+MYSKY-github@users.noreply.github.com>
2023-07-12 19:52:28 +0800

    Update prepare_2d_data.py
    add the code to process label and instance

--- data/scannet-mv/prepare_2d_data.py
+++ data/scannet-mv/prepare_2d_data.py
@@ -34,17 +34,14 @@ except:
 parser = argparse.ArgumentParser()
 parser.add_argument('--scannet_path', required=True, help='path to scannet data')
 parser.add_argument('--output_path', required=True, help='where to output 2d data')
-parser.add_argument('--export_label_images', dest='export_label_images', action='store_true')
-parser.add_argument('--label_type', default='label-filt', help='which labels (label or label-filt)')
 parser.add_argument('--frame_skip', type=int, default=20, help='export every nth frame')
 parser.add_argument('--label_map_file', default='', help='path to scannetv2-labels.combined.tsv (required for label export only)')
+parser.add_argument('--scene_index_file', default='', help='path to scannet_train.txt (required for scene traversal)')
 parser.add_argument('--output_image_width', type=int, default=640, help='export image width')
 parser.add_argument('--output_image_height', type=int, default=480, help='export image height')
 
-parser.set_defaults(export_label_images=False)
 opt = parser.parse_args()
-if opt.export_label_images:
-    assert opt.label_map_file != ''
+assert opt.label_map_file != ''
 print(opt)
 
 
@@ -54,26 +51,31 @@ def print_error(message):
 
 # from https://github.com/ScanNet/ScanNet/tree/master/BenchmarkScripts/2d_helpers/convert_scannet_label_image.py
 def map_label_image(image, label_mapping):
-    mapped = np.copy(image)
-    for k,v in label_mapping.iteritems():
-        mapped[image==k] = v
-    return mapped.astype(np.uint8)
+    out = np.zeros_like(image)
+    for k,v in label_mapping.items():
+        out[image==k] = v
+    return out.astype(np.uint8)
 
 def main():
     if not os.path.exists(opt.output_path):
         os.makedirs(opt.output_path)
 
     label_mapping = None
-    if opt.export_label_images:
-        label_map = util.read_label_mapping(opt.label_map_file, label_from='id', label_to='nyu40id')
+    label_map = util.read_label_mapping(opt.label_map_file, label_from='id', label_to='nyu40id')
 
-    scenes = [d for d in os.listdir(opt.scannet_path) if os.path.isdir(os.path.join(opt.scannet_path, d))]
+    scene_index_file = open(opt.scene_index_file,'r')
+    scenes = scene_index_file.readlines()
+
     print('Found %d scenes' % len(scenes))
     for i in range(len(scenes)):
-        sens_file = os.path.join(opt.scannet_path, scenes[i], scenes[i] + '.sens')
-        label_path = os.path.join(opt.scannet_path, scenes[i], opt.label_type)
-        if opt.export_label_images and not os.path.isdir(label_path):
+        scenes[i] = scenes[i][:-1]
+        sens_file = os.path.join(opt.scannet_path, scenes[i] + '.sens')
+        label_path = os.path.join(opt.scannet_path, scenes[i]+'_2d-label','label')
+        instance_path = os.path.join(opt.scannet_path, scenes[i]+'_2d-instance','instance')
+        if not os.path.isdir(label_path):
             print_error('Error: using export_label_images option but label path %s does not exist' % label_path)
+        if not os.path.isdir(instance_path):
+            print_error('Error: instance path %s does not exist' % instance_path)
         output_color_path = os.path.join(opt.output_path, scenes[i], 'color')
         if not os.path.isdir(output_color_path):
             os.makedirs(output_color_path)
@@ -84,8 +86,11 @@ def main():
         if not os.path.isdir(output_pose_path):
             os.makedirs(output_pose_path)
         output_label_path = os.path.join(opt.output_path, scenes[i], 'label')
-        if opt.export_label_images and not os.path.isdir(output_label_path):
+        if not os.path.isdir(output_label_path):
             os.makedirs(output_label_path)
+        output_instance_path = os.path.join(opt.output_path, scenes[i], 'instance')
+        if not os.path.isdir(output_instance_path):
+            os.makedirs(output_instance_path)
 
         # read and export
         sys.stdout.write('\r[ %d | %d ] %s\tloading...' % ((i + 1), len(scenes), scenes[i]))
@@ -97,13 +102,18 @@ def main():
         sd.export_depth_images(output_depth_path, image_size=[opt.output_image_height, opt.output_image_width], frame_skip=opt.frame_skip)
         sd.export_poses(output_pose_path, frame_skip=opt.frame_skip)
 
-        if opt.export_label_images:
-            for f in range(0, len(sd.frames), opt.frame_skip):
-                label_file = os.path.join(label_path, str(f) + '.png')
-                image = np.array(imageio.imread(label_file))
-                image = sktf.resize(image, [opt.output_image_height, opt.output_image_width], order=0, preserve_range=True)
-                mapped_image = map_label_image(image, label_map)
-                imageio.imwrite(os.path.join(output_label_path, str(f) + '.png'), mapped_image)
+        for f in range(0, len(sd.frames), opt.frame_skip):
+            label_file = os.path.join(label_path, str(f) + '.png')
+            image = np.array(imageio.imread(label_file))
+            image = sktf.resize(image, [opt.output_image_height, opt.output_image_width], order=0, preserve_range=True)
+            mapped_image = map_label_image(image, label_map)
+            imageio.imwrite(os.path.join(output_label_path, str(f) + '.png'), mapped_image)
+
+        for f in range(0, len(sd.frames), opt.frame_skip):
+            instance_file = os.path.join(instance_path, str(f) + '.png')
+            image = np.array(imageio.imread(instance_file))
+            image = sktf.resize(image, [opt.output_image_height, opt.output_image_width], order=0, preserve_range=True)
+            imageio.imwrite(os.path.join(output_instance_path, str(f) + '.png'), image)
     print('')
 
 
