4dc66cee80fab4c34bb35f84753e8d91efd17f0c
xcThu <79625673+MYSKY-github@users.noreply.github.com>
2024-03-06 11:15:03 +0800

    Update test_time_aug.py

--- mmdet3d/datasets/pipelines/test_time_aug.py
+++ mmdet3d/datasets/pipelines/test_time_aug.py
@@ -184,9 +184,7 @@ class MultiScaleFlipAug3D(object):
         """
         aug_data = []
 
-        # modified from `flip_aug = [False, True] if self.flip else [False]`
-        # to reduce unnecessary scenes when using double flip augmentation
-        # during test time
+
         flip_aug = [True] if self.flip else [False]
         pcd_horizontal_flip_aug = [False, True] \
             if self.flip and self.pcd_horizontal_flip else [False]
